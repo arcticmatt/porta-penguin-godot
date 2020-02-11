e5ae5774606d28c85de30b42d7ca8717ad40912d
Matt Lim <>
2020-02-10 21:13:54 -0800

    Add middle separator for instructions

--- scripts/ControlsContainer.gd
+++ scripts/ControlsContainer.gd
@@ -8,5 +8,6 @@ func _ready():
 	var num_times_instructions_shown = Save.get_num_times_instructions_shown()
 	if num_times_instructions_shown < MAX_TIMES_INSTRUCTIONS_SHOWN:
 		Save.save_num_times_instructions_shown(num_times_instructions_shown + 1)
-	elif not g_always_show_instructions:
-		visible = false
+		visible = true
+	else:
+		visible = g_always_show_instructions
