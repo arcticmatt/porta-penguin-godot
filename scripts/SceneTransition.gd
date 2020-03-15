c7eaa042a327352065df0f718c2472737131bc23
Matt Lim <mlimcollege@gmail.com>
2020-03-15 12:18:13 -0700

    Change progress bar animation from rect to rounded bar

--- scripts/SceneTransition.gd
+++ scripts/SceneTransition.gd
@@ -11,21 +11,21 @@ var g_saved_main_node_trump = null
 func change_scene_node(node, inspeed = 1.0, outspeed = 1.0, progress_speed = 1.0, long_fade = false):
 	MainMusicPlayer.stop_all()
 	
-	$AnimationPlayer.play("fade", -1, inspeed)
-	yield($AnimationPlayer, "animation_finished")
+	$FadePlayer.play("fade", -1, inspeed)
+	yield($FadePlayer, "animation_finished")
 
 	var current_scene = self._remove_current_scene()
 	get_tree().get_root().add_child(node)
 	
 	if long_fade:
-		$AnimationPlayer.play("fadeout", -1, outspeed)
+		$FadePlayer.play("fadeout", -1, outspeed)
 	else:
-		$AnimationPlayer.play("fadeout2", -1, outspeed)
+		$FadePlayer.play("fadeout2", -1, outspeed)
 		
 	if progress_speed != null:
 		_play_progress(progress_speed)
 		
-	yield($AnimationPlayer, "animation_finished")
+	yield($FadePlayer, "animation_finished")
 	
 	_play_song_name(node.get_name())
 	
@@ -34,8 +34,8 @@ func change_scene_node(node, inspeed = 1.0, outspeed = 1.0, progress_speed = 1.0
 func change_scene_path(path, inspeed = 1.0, outspeed = 1.0, progress_speed = 1.0, long_fade = false):
 	MainMusicPlayer.stop_all()
 	
-	$AnimationPlayer.play("fade", -1, inspeed)
-	yield($AnimationPlayer, "animation_finished")
+	$FadePlayer.play("fade", -1, inspeed)
+	yield($FadePlayer, "animation_finished")
 
 	var current_scene = self._remove_current_scene()
 	var next_scene_resource = load(path)
@@ -43,14 +43,14 @@ func change_scene_path(path, inspeed = 1.0, outspeed = 1.0, progress_speed = 1.0
 	get_tree().get_root().add_child(next_scene)
 	
 	if long_fade:
-		$AnimationPlayer.play("fadeout", -1, outspeed)
+		$FadePlayer.play("fadeout", -1, outspeed)
 	else:
-		$AnimationPlayer.play("fadeout2", -1, outspeed)
+		$FadePlayer.play("fadeout2", -1, outspeed)
 		
 	if progress_speed != null:
 		_play_progress(progress_speed)
 		
-	yield($AnimationPlayer, "animation_finished")
+	yield($FadePlayer, "animation_finished")
 	
 	_play_song_path(path)
 	
@@ -59,44 +59,44 @@ func change_scene_path(path, inspeed = 1.0, outspeed = 1.0, progress_speed = 1.0
 func change_scene_to(scene, path, inspeed = 1.0, outspeed = 1.0, progress_speed = 1.0, long_fade = false):
 	MainMusicPlayer.stop_all()
 	
-	$AnimationPlayer.play("fade", -1, inspeed)
-	yield($AnimationPlayer, "animation_finished")
+	$FadePlayer.play("fade", -1, inspeed)
+	yield($FadePlayer, "animation_finished")
 	get_tree().change_scene_to(scene)
 	
 	if long_fade:
-		$AnimationPlayer.play("fadeout", -1, outspeed)
+		$FadePlayer.play("fadeout", -1, outspeed)
 	else:
-		$AnimationPlayer.play("fadeout2", -1, outspeed)
+		$FadePlayer.play("fadeout2", -1, outspeed)
 		
 	if progress_speed != null:
 		_play_progress(progress_speed)
 		
-	yield($AnimationPlayer, "animation_finished")
+	yield($FadePlayer, "animation_finished")
 	
 	_play_song_path(path)
 	
 func play_fade_in(speed = 1.0):
-	$AnimationPlayer.play("fade", -1, speed)
-	yield($AnimationPlayer, "animation_finished")
+	$FadePlayer.play("fade", -1, speed)
+	yield($FadePlayer, "animation_finished")
 	
 func play_fade_out(speed = 1.0):
-	$AnimationPlayer.play("fadeout2", -1, speed)
-	yield($AnimationPlayer, "animation_finished")
+	$FadePlayer.play("fadeout2", -1, speed)
+	yield($FadePlayer, "animation_finished")
 
 func _play_progress(speed):
 	thread.start(self, "_play_progress_threaded", speed)
 	thread.wait_to_finish()
 
 func _play_progress_threaded(speed):
-	$WhiteRect.visible = true
-	$GreenRect.visible = true
+	$WhiteButton.visible = true
+	$PinkButton.visible = true
 	
-	$ProgressBarPlayer.play("progress_smooth", -1, speed)
+	$ProgressBarPlayer.play("progress", -1, speed)
 	yield($ProgressBarPlayer, "animation_finished")
 	
-	$WhiteRect.visible = false
-	$GreenRect.visible = false
-	$GreenRect.rect_size = Vector2(0, 62)
+	$WhiteButton.visible = false
+	$PinkButton.visible = false
+	$PinkButton.rect_size = Vector2(0, 62)
 	$ProgressBarPlayer.stop()
 
 func _play_song_path(path):
